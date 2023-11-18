65ae8b5a1095b58295f5829e6c8605a967789f50
naidak <57109234+naidak@users.noreply.github.com>
2023-11-18 23:20:18 +0100

    dodan kontroler za osobu

---
+++ JobSearchingWebApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using JobSearchingWebApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace JobSearchingWebApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Osoba> Osobe { get; set; }
+        public DbSet<Kompanija> Kompanije { get; set; }
+
+        public ApplicationDbContext(
+            DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
