e1d0906a643f46724a33c2dca7ed66877fff6384
Rejhana Nezirić <99076132+rejhana-neziric@users.noreply.github.com>
2023-12-14 23:41:41 +0100

    Update ApplicationDbContext.cs

--- JobSearchingWebApp/Data/ApplicationDbContext.cs
+++ JobSearchingWebApp/Data/ApplicationDbContext.cs
@@ -20,5 +20,15 @@ namespace JobSearchingWebApp.Data
             DbContextOptions options) : base(options)
         {
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys()))
+            {
+                relationship.DeleteBehavior = DeleteBehavior.NoAction;
+            }
+        }
     }
 }
