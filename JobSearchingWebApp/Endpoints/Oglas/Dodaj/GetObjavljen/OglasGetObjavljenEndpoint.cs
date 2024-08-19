df09aa9b1f7122fa5eef22ebd5960ff9002f251a
naidak <57109234+naidak@users.noreply.github.com>
2024-08-19 13:37:57 +0200

    T63 - Objavljivanje oglasa (backend)

---
+++ JobSearchingWebApp/Endpoints/Oglas/Dodaj/GetObjavljen/OglasGetObjavljenEndpoint.cs
@@ -0,0 +1,70 @@
+﻿using Azure.Core;
+using JobSearchingWebApp.Data;
+using JobSearchingWebApp.Endpoints.Oglas.GetAll;
+using JobSearchingWebApp.Helper;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RouteAttribute = Microsoft.AspNetCore.Mvc.RouteAttribute;
+
+namespace JobSearchingWebApp.Endpoints.Oglas.Dodaj.GetObjavljen
+{
+    [Tags("Oglas")]
+    [Route("oglas-get-by-objavljen")]
+    public class OglasGetObjavljenEndpoint : MyBaseEndpoint<bool, OglasGetObjavljenResponse>
+    {
+        private readonly ApplicationDbContext dbContext;
+        public OglasGetObjavljenEndpoint(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext; 
+        }
+        [HttpGet("{objavljen}")]
+        public override async Task<OglasGetObjavljenResponse> MyAction(bool objavljen, CancellationToken cancellationToken)
+        {
+            var oglasi = dbContext.Oglasi.Include(k=> k.Kompanija).AsEnumerable().Where(x=> x.Objavljen == objavljen).ToList();
+
+            var lista = oglasi.Select(oglas => new OglasGetObjavljenResponseOglas
+            {
+                Id = oglas.Id,
+                KompanijaId = oglas.KompanijaId,
+                KompanijaNaziv = oglas.Kompanija?.Naziv?? "N/A",
+                NazivPozicije = oglas.NazivPozicije,
+                DatumObjave = oglas.DatumObjave,
+                TipPosla = oglas.TipPosla,
+                RokPrijave = oglas.RokPrijave,
+                Iskustvo = dbContext.OglasIskustvo
+                        .Where(iskustvo => iskustvo.OglasId == oglas.Id)
+                        .Select(iskustvo => new OglasGetByObjavljenResponseOglasIskustvo
+                        {
+                            Id = iskustvo.IskustvoId,
+                            Naziv = iskustvo.Iskustvo.Naziv
+                        })
+                        .ToList(),
+
+                Lokacija = dbContext.OglasLokacija
+                            .Where(lokacija => lokacija.OglasId == oglas.Id)
+                            .Select(lokacija => new OglasGetByObjavljenResponseOglasLokacija
+                            {
+                                Id = lokacija.LokacijaId,
+                                Naziv = lokacija.Lokacija.Naziv
+                            })
+                            .ToList(),
+                OpisOglasa = dbContext.OpisOglas
+                          .Where(opis => opis.OglasId == oglas.Id)
+                          .Select(opis => new OglasGetByObjavljenResponseOpisOglasa
+                          {
+                              MinimumGodinaIskustva = (int)opis.MinimumGodinaIskustva,
+                              PrefiraneGodineIskstva = (int)opis.PrefiraneGodineIskstva,
+                              Benefiti = opis.Benefiti,
+                              Vjestine = opis.Vjestine,
+                              Kvalifikacija = opis.Kvalifikacija,
+                              OpisPozicije = opis.OpisPozicije
+                          })
+                          .SingleOrDefault()!,
+            }).ToList();
+
+            OglasGetObjavljenResponse oglasGetObjavljenResponse = new OglasGetObjavljenResponse { Oglasi = lista };
+            return oglasGetObjavljenResponse;
+        }
+    }
+}
---
+++ JobSearchingWebApp/Endpoints/Oglas/Dodaj/GetObjavljen/OglasGetObjavljenResponseOglas.cs
@@ -0,0 +1,46 @@
+﻿using JobSearchingWebApp.Endpoints.Oglas.GetById;
+
+namespace JobSearchingWebApp.Endpoints.Oglas.Dodaj.GetObjavljen
+{
+    public class OglasGetObjavljenResponse
+    {
+        public List<OglasGetObjavljenResponseOglas> Oglasi { get; set; }
+    }
+    public class OglasGetObjavljenResponseOglas
+    {
+        public int Id { get; set; }
+        public string KompanijaNaziv { get; set; }
+        public string KompanijaId { get; set; }
+        public string NazivPozicije { get; set; }
+        public DateTime DatumObjave { get; set; }
+        public string Plata { get; set; }
+        public string TipPosla { get; set; }
+        public DateTime RokPrijave { get; set; }
+        public DateTime? DatumModificiranja { get; set; }
+        public List<OglasGetByObjavljenResponseOglasLokacija> Lokacija { get; set; }
+        public List<OglasGetByObjavljenResponseOglasIskustvo> Iskustvo { get; set; }
+        public OglasGetByObjavljenResponseOpisOglasa? OpisOglasa { get; set; }
+    }
+
+    public class OglasGetByObjavljenResponseOpisOglasa
+    {
+        public string OpisPozicije { get; set; }
+        public int MinimumGodinaIskustva { get; set; }
+        public int PrefiraneGodineIskstva { get; set; }
+        public string? Kvalifikacija { get; set; }
+        public string? Vjestine { get; set; }
+        public string? Benefiti { get; set; }
+    }
+
+    public class OglasGetByObjavljenResponseOglasLokacija
+    {
+        public int Id { get; set; }
+        public string Naziv { get; set; }
+    }
+
+    public class OglasGetByObjavljenResponseOglasIskustvo
+    {
+        public int Id { get; set; }
+        public string Naziv { get; set; }
+    }
+}
